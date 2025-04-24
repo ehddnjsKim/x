a6ab8b1a0b49d0ad0c021a7f8946528d7ad09867
ehddnjsKim <kdo316@naver.com>
2025-04-24 12:48:32 +0900

    04/24 오전수업 끝

--- app.mjs
+++ app.mjs
@@ -6,10 +6,10 @@ const app = express();
 
 app.use(express.json());
 
-app.use("./posts", postsRouter);
-app.use("./auth", authRouter);
+app.use("/posts", postsRouter);
+app.use("/auth", authRouter);
 
-app.use((req, res, nex) => {
+app.use((req, res, next) => {
   res.sendStatus(404);
 });
 
--- router/auth.mjs
+++ router/auth.mjs
@@ -3,8 +3,9 @@ import express from "express";
 const router = express.Router();
 
 // 회원가입
-
+router.post("/");
 // 로그인
 
 // 로그인 유지
+
 export default router;
